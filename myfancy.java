c4fe7781525d1f2e182119f4ab7dc3779fc88a38
SHAJ <a.ammar@gmail.com>
2022-05-13 23:00:59 +0600

    X-developer slight changes in java file,it delay for display error

--- myfancy.java
+++ myfancy.java
@@ -2,9 +2,14 @@ public class myfancy {
 
     public static void main(String[] args) {
          
-       int num1 = 5, num2 = 15, sum;
+       int num1 = 5, num2 = 15, sum , sub;
+       
        sum = num1 + num2;
+
+       sub = num1 - num2 || num2 - num1; 
  
        System.out.println("Sum of these numbers: "+sum);
+       System.out.println("Sum of these numbers: "+sub);
+         
     }
  }
